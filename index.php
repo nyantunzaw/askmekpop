a1eb7472b3108c44798301038c8b06532c9be56a
nyantunzaw <nyantunzaw@gmail.com>
2016-11-13 02:08:57 +0800

    Added test codes

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
+if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == "EAACOsds2GjIBAAsNeRHvd7pmWaVGdrPTKrlcRCpwHaXY4ZByiZC7RT2ZAUgSignn1l541HPKAZC5KCIpMQqchZAURNgQekuu0fyaBeDQUUkzIimw2WKSGBqNy65MnCRH7SNyoVvlTYwu5RRDnUrsW1MhHh9GHzKvBKgJRI3AaFQZDZD") {
+    echo $_REQUEST['hub_challenge'];
+} else {
+    $data = json_decode(file_get_contents("php://input"), true);
+    file_put_contents('bot_'.time().'.txt', print_r($data, true));
+}
 
-file_put_contents("fb.txt", file_get_contents("php://input"));
+//file_put_contents("fb.txt", file_get_contents("php://input"));
 
-echo "hello world";
+echo "hello world2";
 
 //$fb = file_get_contents("fb.txt");
 
