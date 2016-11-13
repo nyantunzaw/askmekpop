3411c28dc4b495cf7577d561fbdacab81a4b4485
nyantunzaw <nyantunzaw@gmail.com>
2016-11-13 23:47:02 +0800

    ed

--- index.php
+++ index.php
@@ -236,11 +236,11 @@ if($message)
   }';
 
  curl_setopt($ch, CURLOPT_POST, 1);
+ 
+  curl_setopt($ch, CURLOPT_POST, $json_menu);
 
  curl_setopt($ch, CURLOPT_POSTFIELDS, $json_enc);
 
- curl_setopt($ch, CURLOPT_POSTFIELDS, $json_menu);
- 
  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
 
  if(!empty($input['entry'][0]['messaging'][0]['message'])){
