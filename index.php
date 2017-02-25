d93b701c654422ab0c5819cdae1d10c060c7f7ed
nyantunzaw <nyantunzaw@gmail.com>
2017-02-25 16:30:56 +0800

    web

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ if($message)
 							"buttons": [{
 								"title":"View it now",
 								"type":"web_url",
-								"url":"http://askmekpop.herokuapp.com/game",
+								"url":"http://askmekpop.herokuapp.com/web",
 								"webview_height_ratio":"tall"
 							}],
 							"image_url": "http://askmekpop.herokuapp.com/images/nteezy.png",
